879fbf17dc9f5aa982b3ca9ce9eb22ce451df332
TiveCS <ahmadfathanhanif@gmail.com>
2023-03-17 16:04:47 +0700

    rename import of register page

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 import ErrorPage from './pages/ErrorPage';
 import Home from './pages/Home';
 import UpdateProductPage from './pages/Products/Update';
-import Register from './pages/Register/register';
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
